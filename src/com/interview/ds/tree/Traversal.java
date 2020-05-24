03e0cf104f25acbb920ccc09625dae2917ea63c4
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-05-24 21:18:41 +0530

    Iterative Inorder and PreOrder Traversal

--- src/com/interview/ds/tree/Traversal.java
+++ src/com/interview/ds/tree/Traversal.java
@@ -1,5 +1,7 @@
 package com.interview.ds.tree;
 
+import java.util.Stack;
+
 public class Traversal {
 
 	public static void main(String[] args) {
@@ -11,8 +13,19 @@ public class Traversal {
 		
 		System.out.println();
 		
+		System.out.println("Iterative Inorder Traversal Of Tree : ");
+		inOrderTraversalIterative(root);
+		
+		System.out.println();
+		
 		System.out.println("Preorder Traversal Of Tree : ");
 		preOrderTraversal(root);
+		
+		System.out.println();
+		
+
+		System.out.println("Iterative Preorder Traversal Of Tree : ");
+		preOrderTraversalIterative(root);
 		
 		System.out.println();
 		
@@ -21,6 +34,46 @@ public class Traversal {
 	}
 	
 	
+	private static void inOrderTraversalIterative(TreeNode root) {
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		TreeNode temp = root;
+		while (true) {
+
+			while (temp != null) {
+				s.push(temp);
+				temp = temp.left;
+			}
+
+			if (s.isEmpty())
+				break;
+
+			TreeNode node = s.pop();
+			System.out.print(node.data + " ");
+			temp = node.right;
+
+		}
+	}
+
+	private static void preOrderTraversalIterative(TreeNode root) {
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		TreeNode temp = root;
+		while (true) {
+
+			while (temp != null) {
+				s.push(temp);
+				System.out.print(temp.data + " ");
+				temp = temp.left;
+			}
+
+			if (s.isEmpty())
+				break;
+
+			TreeNode node = s.pop();
+			temp = node.right;
+
+		}
+	}
+
 	public static void inOrderTraversal(TreeNode root) {
 		if(root == null) {
 			return;
