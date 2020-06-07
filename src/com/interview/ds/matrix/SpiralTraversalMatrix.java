b6f26b1361f46fef6d801e5240a19f21f4cab72a
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-07 12:27:16 +0530

    Spiral Traversal Of Matrix. With Output

--- src/com/interview/ds/matrix/SpiralTraversalMatrix.java
+++ src/com/interview/ds/matrix/SpiralTraversalMatrix.java
@@ -3,7 +3,11 @@ package com.interview.ds.matrix;
 public class SpiralTraversalMatrix {
 
 	public static void main(String[] args) {
-		int[][] mat = { { 10, 11, 12 , 13}, { 14, 15, 16 , 17 }, { 18, 19 , 20 ,21} };
+		int[][] mat = { { 10, 11, 12, 13, 14 },
+						{ 15, 16, 17, 18, 19 }, 
+						{ 20, 21, 22, 23, 24 },
+						{ 25, 26, 27, 28, 29 },
+						};
 		System.out.println("Original Matrix : ");
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[0].length; j++) {
@@ -50,3 +54,13 @@ public class SpiralTraversalMatrix {
 	}
 
 }
+
+/*
+ * Original Matrix : 
+10 11 12 13 14 
+15 16 17 18 19 
+20 21 22 23 24 
+25 26 27 28 29 
+Sprial Traversal Of Matrix : 
+10 11 12 13 14 19 24 29 28 27 26 25 20 15 16 17 18 23 22 21 
+ * */
