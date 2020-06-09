9ce309be5d8eb70ddde1ff9caf2629cb08b51842
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-09 18:27:59 +0530

    Updated With Recursive Search

--- src/com/interview/ds/trie/InsertWordsInTrie.java
+++ src/com/interview/ds/trie/InsertWordsInTrie.java
@@ -1,5 +1,7 @@
 package com.interview.ds.trie;
 
+import javax.naming.directory.SearchResult;
+
 public class InsertWordsInTrie {
 
 	public static void main(String[] args) {
@@ -16,19 +18,19 @@ public class InsertWordsInTrie {
 		insert(root , str3);
 		insert(root, str5);
 		
-		boolean doesExist = checkWord(root,str1);
+		boolean doesExist = search(root,str1);
 		System.out.println("Does Word : "+str1+" exists : "+doesExist);
-		doesExist = checkWord(root,str1);
+		doesExist = search(root,str1);
 		System.out.println("Does Word : "+str2+" exists : "+doesExist);
-		doesExist = checkWord(root,str1);
+		doesExist = search(root,str1);
 		System.out.println("Does Word : "+str3+" exists : "+doesExist);
-		doesExist = checkWord(root,str4);
+		doesExist = search(root,str4);
 		System.out.println("Does Word : "+str4+" exists : "+doesExist);
-		doesExist = checkWord(root,str5);
+		doesExist = searchRecursive(root,str5);
 		System.out.println("Does Word : "+str5+" exists : "+doesExist);
 	}
 
-	private static boolean checkWord(TrieNode root, String str) {
+	private static boolean search(TrieNode root, String str) {
 		TrieNode temp = root;
 		for (int i = 0; i < str.length(); i++) {
 			if(temp.children.containsKey(str.charAt(i))) {
@@ -42,47 +44,66 @@ public class InsertWordsInTrie {
 	}
 
 	private static void insert(TrieNode root, String str) {
-		
-		TrieNode temp = root; 
-		
-		for(int i = 0; i < str.length() ; i++) {
-			
-			if(temp.children.containsKey(str.charAt(i))) {
+
+		TrieNode temp = root;
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (temp.children.containsKey(str.charAt(i))) {
 				temp = temp.children.get(str.charAt(i));
 			}
-			
+
 			else {
 				temp.children.put(str.charAt(i), new TrieNode());
 				temp.endOfWord = false;
 				temp = temp.children.get(str.charAt(i));
 			}
 		}
-		
+
 		temp.endOfWord = true;
-		
+
 	}
 	
-	public static void insertRecursive(TrieNode root , String word) {
-        insertRecursive(root, word, 0);
-    }
+	public static void insertRecursive(TrieNode root, String word) {
+		insertRecursive(root, word, 0);
+	}
 
 
-    private static void insertRecursive(TrieNode temp, String word, int index) {
-        if (index == word.length()) {
-            temp.endOfWord = true;
-            return;
-        }
-        char ch = word.charAt(index);
-        TrieNode node = temp.children.get(ch);
+	private static void insertRecursive(TrieNode temp, String word, int index) {
+		if (index == word.length()) {
+			temp.endOfWord = true;
+			return;
+		}
+		char ch = word.charAt(index);
+		TrieNode node = temp.children.get(ch);
 
-        if (node == null) {
-            node = new TrieNode();
-            temp.children.put(ch, node);
-        }
-        insertRecursive(node, word, index + 1);
-    }
+		if (node == null) {
+			node = new TrieNode();
+			temp.children.put(ch, node);
+		}
+		insertRecursive(node, word, index + 1);
+	}
+    
+	public static boolean searchRecursive(TrieNode root, String word) {
+		return searchRecursive(root, word, 0);
+	}
+    
+	private static boolean searchRecursive(TrieNode temp, String word, int index) {
+		if (index == word.length()) {
+			return temp.endOfWord;
+		}
+		char ch = word.charAt(index);
+		TrieNode node = temp.children.get(ch);
+		if (node == null) {
+			return false;
+		}
+		return searchRecursive(node, word, index + 1);
+	}
 
 }
+
+
+
 
 /*
  *Does Word : abcd exists : true
