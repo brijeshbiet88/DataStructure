d07d42fef95aac4cdeeaceec2975fe9c53c7f214
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-14 13:32:36 +0530

    Buy Sell Stock Problem With 1 Transactions To Generate Maximum Profit

---
+++ src/com/interview/ds/array/BuySellStock1Transaction.java
@@ -0,0 +1,70 @@
+package com.interview.ds.array;
+
+public class BuySellStock1Transaction {
+
+	public static void main(String[] args) {
+		System.out.println("Test Case 1 :\n");
+		int [] price = {31 , 33 , 38 , 46 , 32 , 28 , 22 , 29 , 37 , 30 , 40 };
+		int maxProfit = findMaxProfit(price , price.length);
+		System.out.println("Max Profit In Buying & Sellings Stocks in 1 Transaction is "+maxProfit);
+		System.out.println("\n");
+		System.out.println("Test Case 2 :\n");
+		int [] price2 = {21 , 20 , 19 , 18 , 15};
+		maxProfit = findMaxProfit(price2 , price2.length);
+		System.out.println("Max Profit In Buying & Sellings Stocks in 1 Transaction is "+maxProfit);
+		
+		System.out.println("\n");
+		
+		System.out.println("Test Case 2 :\n");
+		int [] price3 = {21 , 20 , 19 , 18 , 25};
+		maxProfit = findMaxProfit(price3 , price3.length);
+		System.out.println("Max Profit In Buying & Sellings Stocks in 1 Transaction is "+maxProfit);
+	}
+
+	private static int findMaxProfit(int[] price, int n) {
+		if (n < 2) {
+			System.out.println("Invalid Input ");
+			System.exit(0);
+		}
+		int maxProfit = 0;
+		int localMinima = 0, localMaxima = 0 ;
+		int i = 0;
+
+		while (i < n - 1) {
+			while (i < n - 1) {
+				if (price[i] < price[i + 1]) {
+					localMinima = i;
+					break;
+				}
+				i++;
+			}
+
+			while (i < n - 1) {
+				if (price[i] > price[i + 1]) {
+					localMaxima = i;
+					break;
+				}
+				
+				if(i == n - 2 && price[n-2] < price[n-1]) {
+					localMaxima = n-1;
+				}
+				
+				i++;
+			}
+
+			if (localMinima < localMaxima && maxProfit < (price[localMaxima] - price[localMinima])) {
+					maxProfit = price[localMaxima] - price[localMinima];
+					System.out.println("Buy at " + price[localMinima] + " and sell at " + price[localMaxima]);
+			}
+		}
+		return maxProfit;
+	}
+
+}
+/*
+Buy at 21 and sell at 46
+Buy at 22 and sell at 37
+Buy at 30 and sell at 40
+Max Profit In Buying & Sellings Stocks in 3 Transactions is 50
+ 
+ */
