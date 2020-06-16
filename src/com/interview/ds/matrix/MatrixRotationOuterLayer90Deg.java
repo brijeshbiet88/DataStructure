3bee45f3c1eb0d7c1b7cdecb63f7c11481e8e448
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-16 21:01:14 +0530

    Program To Rotate Outer Layer Of Square Matrix By 90 Degree

---
+++ src/com/interview/ds/matrix/MatrixRotationOuterLayer90Deg.java
@@ -0,0 +1,81 @@
+package com.interview.ds.matrix;
+
+public class MatrixRotationOuterLayer90Deg {
+
+	public static void main(String[] args) {
+		int[][] mat = { { 10, 11, 12, 13},
+						{ 14, 15, 16, 17},
+						{ 18, 19, 20, 21},
+						{ 22, 23, 24, 25}, };
+		
+		System.out.println("Original Matrix : ");
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[0].length; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		rotateByOnePace(mat);
+		
+		System.out.println("Matrix After Rotation Of Outer Layer By 90 Degree :");
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[0].length; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void rotateByOnePace(int[][] mat) {
+		if (mat.length == 0) {
+			System.out.println("Empty Matrix ");
+			return;
+		}
+
+		int rows = mat.length, cols = mat[0].length;
+
+		int rf = 0, cf = 0, rl = rows - 1, cl = cols - 1;
+
+		for(int count = 1 ; count < mat.length ; count++) {
+		int a = mat[rf][0];
+		int b;
+ 		
+		for (int i = cf; i < cl; i++) {
+			b = mat[rf][i + 1];
+			mat[rf][i + 1] = a;
+
+			a = a + b;
+			b = a - b;
+			a = a - b;
+		}
+
+		for (int i = rf; i < rl; i++) {
+			b = mat[i + 1][cl];
+			mat[i + 1][cl] = a;
+
+			a = a + b;
+			b = a - b;
+			a = a - b;
+		}
+		for (int i = cl; i > cf; i--) {
+			b = mat[rl][i - 1];
+			mat[rl][i - 1] = a;
+
+			a = a + b;
+			b = a - b;
+			a = a - b;
+		}
+		for (int i = rl; i > rf; i--) {
+			b = mat[i - 1][rf];
+			mat[i - 1][rf] = a;
+
+			a = a + b;
+			b = a - b;
+			a = a - b;
+		}
+
+	}
+	}
+
+}
