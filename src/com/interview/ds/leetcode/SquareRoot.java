f484d4c698dbc3acdfe56c172b91294b31b23ac1
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-18 00:13:53 +0530

    Square Root Of A Number Using Bnary Search. Floor Value in case of not perfect Square

--- src/com/interview/ds/leetcode/SquareRoot.java
+++ src/com/interview/ds/leetcode/SquareRoot.java
@@ -1,28 +1,40 @@
 package com.interview.ds.leetcode;
 
-import java.util.Scanner;
-
 public class SquareRoot {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter The Element : ");
-		int num = sc.nextInt();
-		int start = 0 , high = num;
-		System.out.println("\nSquare Root of Number is : "+findSquareRoot(start , high, num));
+		for (int num = 4; num <= 16; num++) {
+			int start = 0, high = num;
+			System.out.println("Square Root of Number "+num +" is : " + findSquareRoot(start, high, num));
+		}
 	}
 
-	private static int findSquareRoot(int low, int high , int num) {
+	private static int findSquareRoot(int low, int high, int num) {
 		int mid = (low + high) / 2;
 
 		if (mid * mid == num || mid == low)
 			return mid;
 
 		if (mid * mid > num)
-			return findSquareRoot(low, mid , num);
+			return findSquareRoot(low, mid, num);
 
 		else
-			return findSquareRoot(mid, high , num );
+			return findSquareRoot(mid, high, num);
 	}
 
 }
+/*
+Square Root of Number 4 is : 2
+Square Root of Number 5 is : 2
+Square Root of Number 6 is : 2
+Square Root of Number 7 is : 2
+Square Root of Number 8 is : 2
+Square Root of Number 9 is : 3
+Square Root of Number 10 is : 3
+Square Root of Number 11 is : 3
+Square Root of Number 12 is : 3
+Square Root of Number 13 is : 3
+Square Root of Number 14 is : 3
+Square Root of Number 15 is : 3
+Square Root of Number 16 is : 4
+*/
