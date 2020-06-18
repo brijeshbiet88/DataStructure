49077dd09be66a7ccde4ac1d281375759e5610e4
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-18 23:13:47 +0530

    Rain Trapping Problem

---
+++ src/com/interview/ds/leetcode/RainTrappingProblem.java
@@ -0,0 +1,49 @@
+package com.interview.ds.leetcode;
+
+public class RainTrappingProblem {
+
+	public static void main(String[] args) {
+		int[] a = { 5, 2, 4, 3, 1, 6};
+		System.out.println("Water Trapped : "+findWaterTrapped(a));
+
+	}
+
+	private static int findWaterTrapped(int[] a) {
+		int result = 0;
+		
+		int [] maxRight = new int[a.length];
+		int [] maxLeft = new int[a.length];
+		
+		int max = Integer.MIN_VALUE;
+		for(int i = a.length -1 ; i >= 0 ; i--) {
+			if(max < a[i]) {
+				max = a[i];
+			}
+			
+			maxRight[i] = max;
+		}
+		
+		max = Integer.MIN_VALUE;
+		for(int i = 0 ; i < a.length ; i++) {
+			if(max < a[i]) {
+				max = a[i];
+			}
+			
+			maxLeft[i] = max;
+		}
+		
+		for(int i = 1 ; i < a.length -1 ; i++) {
+		  if(maxLeft[i] > maxRight[i]) {
+			  result = result + maxRight[i] -a [i];
+		  }
+		  else {
+			  result = result + maxLeft[i] -a [i];
+		  }
+		}
+		
+		
+		
+		return result ;
+	}
+
+}
