8eb7f5af41335c06efdfcc6685f598713ddbd075
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-06-24 00:11:57 +0530

    Egg Dropping Problem Using Dynamic Programming

---
+++ src/com/interview/ds/dp/EggDropping.java
@@ -0,0 +1,39 @@
+package com.interview.ds.dp;
+
+public class EggDropping {
+
+	public static void main(String[] args) {
+		EggDropping ed = new EggDropping();
+        int result = ed.calculate(2,10);
+        System.out.println("Minimum No of Attempts in WorstCase To Find Floor Where Egg will Break From : "+result);
+
+	}
+	
+	public int calculate(int eggs, int floors) {
+
+		int a[][] = new int[eggs + 1][floors + 1];
+		int c = 0;
+		
+		/* For 1 egg minimum number of attempts will be equlas to number of floors*/
+		for (int i = 0; i <= floors; i++) {
+			a[1][i] = i;
+		}
+
+		for (int e = 2; e <= eggs; e++) {
+			for (int f = 1; f <= floors; f++) {
+				a[e][f] = Integer.MAX_VALUE;
+				for (int k = 1; k <= f; k++) {
+					/* max of if egg breaks then egg will egg-- and floor will be k-- floors
+					   if egg does not break then we have same no of eggs and floor - k floors left
+					 */
+					c = 1 + Math.max(a[e - 1][k - 1], a[e][f - k]);
+					if (c < a[e][f]) {
+						a[e][f] = c;
+					}
+				}
+			}
+		}
+		return a[eggs][floors];
+	}
+
+}
