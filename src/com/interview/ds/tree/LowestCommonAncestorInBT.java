936e987fa0a2bafb2b7cf6814c320e9fa914799d
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-07-05 19:39:43 +0530

    Lowest Common Ancestor Using Iterative Traversal

--- src/com/interview/ds/tree/LowestCommonAncestorInBT.java
+++ src/com/interview/ds/tree/LowestCommonAncestorInBT.java
@@ -1,5 +1,7 @@
 package com.interview.ds.tree;
 
+import java.util.Stack;
+
 public class LowestCommonAncestorInBT {
 
 	public static void main(String[] args) {
@@ -16,6 +18,13 @@ public class LowestCommonAncestorInBT {
 		else
 		{	
 			lca = null;
+			System.out.println("LCA of Nodes "+node1 +" and "+node2+" is : "+lca);
+		}
+		
+		System.out.println("Using Depth First Search ......");
+		
+		lca = findLcaIter(root, node1, node2);
+		if(lca != null) {
 			System.out.println("LCA of Nodes "+node1 +" and "+node2+" is : "+lca);
 		}
 	}
@@ -40,6 +49,65 @@ public class LowestCommonAncestorInBT {
 		
 		if(right != null && left != null) return root;
 		else return left != null ? left : right;
+	}
+	
+	private static TreeNode findLcaIter(TreeNode root, TreeNode node1 , TreeNode node2 ) {
+		Stack<TreeNode> s1 = findElement(root, node1);
+		Stack<TreeNode> s2 = findElement(root, node2);
+		TreeNode lca = null;
+		
+		while(s1.size() > 0 && s2.size() > 0) {
+			
+			if(s1.peek() == s2.peek()) {
+				lca =  s1.peek();
+				return lca;
+			}
+			
+			else if(s1.size() == s2.size()) {
+				s1.pop();
+				s2.pop();
+			}
+			
+			else {
+				if(s1.size() > s2.size()) {
+					s1.pop();
+				}
+				else
+				{
+					s2.pop();	
+				}
+			}
+		}
+		return lca;
+	}
+	
+	
+	private static Stack<TreeNode> findElement(TreeNode root, TreeNode node) {
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		if (root == null)
+			return stack;
+		TreeNode pre = null;
+		
+		while (root != null || !stack.empty()) {
+			if (root != null) {
+				if(root == node) {
+					return stack;
+				}
+				stack.push(root);
+				root = root.left;
+			} else {
+				root = stack.peek();
+				if (root.right == null || root.right == pre) {
+					
+					stack.pop();
+					pre = root;
+					root = null;
+				} else
+					root = root.right;
+			}
+		}
+		return stack;
+	
 	}
 
 }
