04cd7cb1d5e5d1121109f72e20a8fad6b0833913
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-06 00:44:03 +0530

    Cousin in Binary Tree. Updated Output

--- src/com/interview/ds/tree/CousinsInTree.java
+++ src/com/interview/ds/tree/CousinsInTree.java
@@ -21,6 +21,10 @@ public class CousinsInTree {
 		n1 = 1 ;n2 = 3;
 		System.out.println("Are Nodes "+n1+" and "+n2+" Cousins : "+areCousins(root, n1 , n2));
 		
+		System.out.println("-----------Test Case4---------------");
+		n1 = 11 ;n2 = 8;
+		System.out.println("Are Nodes "+n1+" and "+n2+" Cousins : "+areCousins(root, n1 , n2));
+		
 	}
 
 
@@ -34,7 +38,7 @@ public class CousinsInTree {
 		while (!q.isEmpty()) {
 			int n = q.size();
 			for (int i = 0; i < n; i++) {
-				
+
 				temp = q.poll();
 				if (temp.left != null) {
 					q.add(temp.left);
@@ -44,19 +48,19 @@ public class CousinsInTree {
 					q.add(temp.right);
 				}
 				list.add(temp.data);
-				if(temp.left != null && temp.right != null) {
-				  if(temp.left.data == x && temp.right.data == y) {
-					  return false;
-				  }
-				  if(temp.left.data == y && temp.right.data == x) {
-					  return false;
-				  }
+				if (temp.left != null && temp.right != null) {
+					if (temp.left.data == x && temp.right.data == y) {
+						return false;
+					}
+					if (temp.left.data == y && temp.right.data == x) {
+						return false;
+					}
 
 				}
-				
+
 			}
-			
-			if(list.contains(x) && list.contains(y)) {
+
+			if (list.contains(x) && list.contains(y)) {
 				return true;
 			}
 			list.clear();
@@ -66,3 +70,18 @@ public class CousinsInTree {
 
 	}
 }
+
+/*
+ 			1 
+	   2             3 
+  4        5     6        7 
+ 8 9     10 11 12 13    14 15 
+-----------Test Case 1---------------
+Are Nodes 4 and 7 Cousins : true
+-----------Test Case 2---------------
+Are Nodes 4 and 5 Cousins : false
+-----------Test Case3---------------
+Are Nodes 1 and 3 Cousins : false
+-----------Test Case4---------------
+Are Nodes 11 and 8 Cousins : true
+ * */
