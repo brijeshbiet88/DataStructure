cda2437895a4187d2bef013b2abeb08a78306d30
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-03-13 22:23:00 +0530

    Total no of ways to reach nth stair

---
+++ src/com/interview/ds/dp/TotalWaysToReachNthStair.java
@@ -0,0 +1,37 @@
+package com.interview.ds.dp;
+
+public class TotalWaysToReachNthStair {
+
+	public static void main(String[] args) {
+		System.out.println("------------------Test Case 1------------------");
+		int n = 5;
+		int noOfWays = findNoOfWays(n);
+		System.out.println("Total Number Of Ways To Reach "+n+"th Stair "+noOfWays);
+		System.out.println();
+		
+		System.out.println("------------------Test Case 2------------------");
+		n = 4;
+		noOfWays = findNoOfWays(n);
+		System.out.println("Total Number Of Ways To Reach "+n+"th Stair "+noOfWays);
+		System.out.println();
+		
+		System.out.println("------------------Test Case 3------------------");
+		n = 3;
+		noOfWays = findNoOfWays(n);
+		System.out.println("Total Number Of Ways To Reach "+n+"rd Stair "+noOfWays);
+		System.out.println();
+
+	}
+
+	private static int findNoOfWays(int n) {
+		int [] results = new int [n +1 ];
+		results[0] = 1;
+		results[1] = 1;
+		
+		for(int i = 2 ; i <=n ; i++) {
+			results[i] = results[i-2]+ results[i-1];
+		}
+		return results[n];
+	}
+
+}
